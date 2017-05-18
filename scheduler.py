bdc76cfd7ac0d66f6b87e2a9108007564d83f2d9
mediastore93 <mediastore93@gmail.com>
2017-05-18 10:26:33 -0700

    Create scheduler.py

---
+++ scheduler.py
@@ -0,0 +1,8 @@
+dateSTR = datetime.datetime.now().strftime("%H:%M:%S" )
+if dateSTR == ("20:32:10"):
+   #do function
+    print(dateSTR)
+else:
+    # do something useful till this time
+    time.sleep(1)
+    pass
