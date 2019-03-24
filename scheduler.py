671bd63ea56ae86f7b74ab991ab7528ba7393546
mediastore93 <mediastore93@gmail.com>
2019-03-23 18:05:18 -0700

    Delete scheduler.py

--- scheduler.py
+++
@@ -1,8 +0,0 @@
-dateSTR = datetime.datetime.now().strftime("%H:%M:%S" )
-if dateSTR == ("20:32:10"):
-   #do function
-    print(dateSTR)
-else:
-    # do something useful till this time
-    time.sleep(1)
-    pass
